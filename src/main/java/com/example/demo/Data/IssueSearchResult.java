a6b28036900e7ba8b3ea0cb8e9aa033f52310fa3
upeksha <upekshaiwanthi@gmail.com>
2018-02-28 16:29:00 +0530

    add new modules/branches

--- src/main/java/com/example/demo/Data/IssueSearchResult.java
+++ src/main/java/com/example/demo/Data/IssueSearchResult.java
@@ -1,5 +1,44 @@
 package com.example.demo.Data;
 
-public class IssueSearchResult {
+import com.example.demo.persistence.FunctionalArea;
 
+import java.util.List;
+
+public class IssueSearchResult {
+    private String classPath;
+    private String module;
+    private List<FunctionalArea> functionalAreas;
+    private List<String> jiraIssueIds;
+
+    public String getClassPath() {
+        return classPath;
+    }
+
+    public void setClassPath(String classPath) {
+        this.classPath = classPath;
+    }
+
+    public String getModule() {
+        return module;
+    }
+
+    public void setModule(String module) {
+        this.module = module;
+    }
+
+    public List<FunctionalArea> getFunctionalAreas() {
+        return functionalAreas;
+    }
+
+    public void setFunctionalAreas(List<FunctionalArea> functionalAreas) {
+        this.functionalAreas = functionalAreas;
+    }
+
+    public List<String> getJiraIssueIds() {
+        return jiraIssueIds;
+    }
+
+    public void setJiraIssueIds(List<String> jiraIssueIds) {
+        this.jiraIssueIds = jiraIssueIds;
+    }
 }
